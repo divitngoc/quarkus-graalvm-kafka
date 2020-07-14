43b12fd19f0db006d55c3537e8e9e6b6c1e74c2e
divit <divit@host.docker.internal>
2020-07-14 13:35:02 +0100

    Add Collaborator API and Swagger
    Just meant to be a playing ground for us.

--- quarkus-api-server/src/main/java/com/divitngoc/QuarkusApplication.java
+++ quarkus-api-server/src/main/java/com/divitngoc/QuarkusApplication.java
@@ -17,7 +17,7 @@ import io.quarkus.runtime.annotations.QuarkusMain;
  * 
  */
 @QuarkusMain
-public class ApiApplication {
+public class QuarkusApplication {
 
     public static void main(String[] args) {
         Quarkus.run(args);
